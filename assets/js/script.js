e236ccb9eba8d8775bd0ad6127c7dc74eca82a7d
Pierre Monvoisin <monvoisin.pierre@gmail.com>
2019-11-16 16:03:47 +0100

    Début de collection de données des articles en JQuery, ajout de bouttons sur les cartes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,15 @@
 $(function(){
-  
+  var articleName = '', articleRef = '', articlePrice = '';
+  var panier = {nameOfArticle : articleName, refOfArticle : articleRef, priceOfArticle : articlePrice}, wishlist = []
+  $('.boutonAchatPanier').click(function(){
+    articleName = $(this).parent().parent().siblings('.card-body').children('.designation').text()
+    articleRef = ($(this).parent().parent().siblings('.card-body').children('h6').text()).slice(4)
+    articlePrice = $(this).parent().siblings('.price').text()
+    console.log(articleName + ' ' + articleRef + ' ' + articlePrice)
+    // Ne fonctionne pas
+    panier = {nameOfArticle : articleName, refOfArticle : articleRef, priceOfArticle : articlePrice};
+    //
+    $('.articleName').text(articleName), $('.articleRef').text('#' + articleRef), $('.articlePrice').text(articlePrice);
+    alert(panier)
+  });
 });
